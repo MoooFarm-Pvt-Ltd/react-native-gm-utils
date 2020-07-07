b2bff026c7d25dd7a0ed3126991038241c6033cd
Gurpreet Singh <gurpreetmundi777@gmail.com>
2020-07-07 09:45:57 +0530

    added few methods
    backToForeground
    unlock
    askOverlayPermission

--- android/src/main/java/com/reactlibrary/GmUtilsModule.java
+++ android/src/main/java/com/reactlibrary/GmUtilsModule.java
@@ -24,4 +24,81 @@ public class GmUtilsModule extends ReactContextBaseJavaModule {
         // TODO: Implement some actually useful functionality
         callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
     }
+
+    private Context getAppContext() {
+        return this.reactContext.getApplicationContext();
+    }
+
+    @ReactMethod
+    public void askForOverlayPermissions() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            Context context = getAppContext();
+            String packageName = context.getApplicationContext().getPackageName();
+
+            Intent myIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + context.getPackageName()));
+            myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+            this.getAppContext().startActivity(myIntent);
+        }
+    }
+
+    @ReactMethod
+    public void backToForeground() {
+        Context context = getAppContext();
+        String packageName = context.getApplicationContext().getPackageName();
+        Intent focusIntent = context.getPackageManager().getLaunchIntentForPackage(packageName).cloneFilter();
+        Activity activity = getCurrentActivity();
+        boolean isOpened = activity != null;
+        Log.d(TAG, "backToForeground, PackageName" + packageName);
+        Log.d(TAG, "backToForeground, app isOpened ?" + (isOpened ? "true" : "false"));
+
+        if (isOpened) {
+            focusIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            activity.startActivity(focusIntent);
+            Log.d(TAG, "backToForeground, Start from background");
+        } else {
+            focusIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK +
+                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED +
+                    WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD +
+                    WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON +
+                    WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON +
+                    Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+
+            getReactApplicationContext().startActivity(focusIntent);
+
+            Log.d(TAG, "backToForeground, Start from Killed");
+        }
+    }
+    
+    @ReactMethod
+    public void unlock() {
+        Log.d(TAG, "manualTurnScreenOn()");
+        UiThreadUtil.runOnUiThread(new Runnable() {
+            public void run() {
+                Activity mCurrentActivity = getCurrentActivity();
+                if (mCurrentActivity == null) {
+                    Log.d(TAG, "ReactContext doesn't hava any Activity attached.");
+                    return;
+                }
+                KeyguardManager keyguardManager = (KeyguardManager) reactContext.getSystemService(Context.KEYGUARD_SERVICE);
+                KeyguardManager.KeyguardLock keyguardLock = keyguardManager.newKeyguardLock(Context.KEYGUARD_SERVICE);
+                keyguardLock.disableKeyguard();
+
+                PowerManager powerManager = (PowerManager) reactContext.getSystemService(Context.POWER_SERVICE);
+                PowerManager.WakeLock wakeLock = powerManager.newWakeLock(
+                        PowerManager.FULL_WAKE_LOCK
+                        | PowerManager.ACQUIRE_CAUSES_WAKEUP
+                        | PowerManager.SCREEN_BRIGHT_WAKE_LOCK
+                        | PowerManager.ON_AFTER_RELEASE, "RNUnlockDeviceModule");
+
+                wakeLock.acquire();
+
+                Window window = mCurrentActivity.getWindow();
+                window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+            }
+        });
+    }
 }
